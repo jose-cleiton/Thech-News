673d49b585fa4b9c44e1498ec5831b792a7b27c7
jose-cleiton <cleitons835@gmail.com>
2022-11-09 20:05:10 -0300

    fix: :bug: corrigindo funçao fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,18 +2,14 @@ import time
 
 from requests import HTTPError, ReadTimeout, get
 
-# from parsel import Selector
-# from tech_news.database import create_news
 # Requisito 1
 URL_BASE = "https://blog.betrybe.com"
-HEADRS = {"user-agent": "Fake user-agent"}
 
 
-# Requisito 1
-def fetch(url: str, timeout: int = 3) -> str | None:
+def fetch(ur, timeout: int = 3):
     try:
         time.sleep(1)
-        response = get(url, headers=HEADRS, timeout=timeout)
+        response = get(ur, headers={"User-Agent": "Chrome"}, timeout=timeout)
         response.raise_for_status()
     except (HTTPError, ReadTimeout):
         return None
